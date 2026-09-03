f9884000c7f3617ed9ba648bab8c6dfdc57dad78
sowmiyakannan8582 <kbsowmiyakannan@gmail.com>
2026-09-03 11:54:44 +0530

    Add files via upload

---
+++ frstprogram/Task_10.java
@@ -0,0 +1,15 @@
+package frstprogram;
+import java.util.Scanner;
+
+public class Task_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s= new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		System.out.println("area of triangle :" + 1.0/2* a*b);
+
+	}
+
+}
